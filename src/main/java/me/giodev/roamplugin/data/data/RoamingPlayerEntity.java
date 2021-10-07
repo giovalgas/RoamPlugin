73b4c427a0612321298a68e4abe15ea46847c490
giovalgas <giovalgascom@gmail.com>
2021-10-07 18:39:48 -0300

    Fix player not taking damage while in roaming mode

--- src/main/java/me/giodev/roamplugin/data/data/RoamingPlayerEntity.java
+++ src/main/java/me/giodev/roamplugin/data/data/RoamingPlayerEntity.java
@@ -6,6 +6,7 @@ import com.cryptomorin.xseries.XItemStack;
 import com.cryptomorin.xseries.XMaterial;
 import de.tr7zw.changeme.nbtapi.NBTContainer;
 import de.tr7zw.changeme.nbtapi.NBTEntity;
+import de.tr7zw.changeme.nbtapi.NBTItem;
 import org.bukkit.Location;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.ConfigurationSection;
@@ -32,17 +33,23 @@ public class RoamingPlayerEntity {
     this.entity = (Skeleton) loc.getWorld().spawnEntity(loc, EntityType.SKELETON);
 
     NBTEntity nbtEntity = new NBTEntity(entity);
-    nbtEntity.mergeCompound(new NBTContainer("{Invulnerable:1,NoAI:1}"));
+    nbtEntity.mergeCompound(new NBTContainer("{NoAI:1}"));
 
     entity.setCustomName("Roaming - " + player.getName());
     entity.getEquipment().setArmorContents(player.getEquipment().getArmorContents());
     entity.getEquipment().setItemInHand(player.getItemInHand());
-    entity.getEquipment().setHelmet(SkullUtils.getSkull(this.owner.getUniqueId()));
+    entity.getEquipment().setHelmet(this.getPlayerHead());
 
   }
 
   public void killEntity() {
     entity.remove();
+  }
+
+  public ItemStack getPlayerHead() {
+    NBTItem nbtItem = new NBTItem(SkullUtils.getSkull(this.owner.getUniqueId()));
+    nbtItem.setString("ROAMER_UUID", owner.getUniqueId().toString());
+    return nbtItem.getItem();
   }
 
   public Location getLocation() {
--- src/main/java/me/giodev/roamplugin/listeners/RoamEntityDamageListener.java
+++ src/main/java/me/giodev/roamplugin/listeners/RoamEntityDamageListener.java
@@ -1,6 +1,9 @@
 package me.giodev.roamplugin.listeners;
 
+import com.cryptomorin.xseries.SkullUtils;
+import de.tr7zw.changeme.nbtapi.NBTItem;
 import me.giodev.roamplugin.RoamPlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
@@ -8,6 +11,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.meta.SkullMeta;
+
+import java.util.UUID;
 
 public class RoamEntityDamageListener implements Listener {
 
@@ -21,6 +26,8 @@ public class RoamEntityDamageListener implements Listener {
   @EventHandler
   public void onEntityDamage(EntityDamageEvent event) {
 
+    plugin.getLog().info(event.getEntity().getType().toString());
+
     Entity entity = event.getEntity();
 
     if(entity.getCustomName() == null) return;
@@ -28,8 +35,13 @@ public class RoamEntityDamageListener implements Listener {
     if(entity.getCustomName().startsWith("Roaming")) {
 
       Skeleton roamerEntity = (Skeleton) entity;
-      SkullMeta sm = (SkullMeta) roamerEntity.getEquipment().getHelmet().getItemMeta();
-      Player roamer = sm.getOwningPlayer().getPlayer();
+      NBTItem nbtItem = new NBTItem(roamerEntity.getEquipment().getHelmet());
+
+      plugin.getLog().info(nbtItem.getString("ROAMER_UUID"));
+
+      if(!nbtItem.hasKey("ROAMER_UUID")) return;
+
+      Player roamer = Bukkit.getPlayer(UUID.fromString(nbtItem.getString("ROAMER_UUID")));
 
       plugin.flipRoamingState(roamer);
       roamer.damage(event.getDamage());
